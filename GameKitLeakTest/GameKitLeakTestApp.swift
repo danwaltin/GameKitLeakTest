399aa002555936b941ea6ca3057e3de08331034b
Dan Waltin <dan@waltin.se>
2023-03-03 20:25:11 +0100

    Added XCode project

---
+++ GameKitLeakTest/GameKitLeakTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameKitLeakTestApp.swift
+//  GameKitLeakTest
+//
+//  Created by Dan Waltin on 2023-03-03.
+//
+
+import SwiftUI
+
+@main
+struct GameKitLeakTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
