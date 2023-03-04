ddcc7962f8d095225b5f43d74548b99c533bbeeb
Dan Waltin <dan@waltin.se>
2023-03-04 09:01:28 +0100

    Added leaking example code

--- GameKitLeakTest/ContentView.swift
+++ GameKitLeakTest/ContentView.swift
@@ -6,17 +6,36 @@
 //
 
 import SwiftUI
+import GameKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	var body: some View {
+		Button("Leak memory") {
+			leakyVariant()
+		}
+		.padding()
+		Button("Does not leak memory") {
+			nonLeakyVariant()
+		}
+		.padding()
+	}
+	
+	private func leakyVariant() {
+		let _ = GKGridGraph(
+			fromGridStartingAt: simd_int2(0, 0),
+			width: 10,
+			height: 10,
+			diagonalsAllowed: false)
+	}
+
+	private func nonLeakyVariant() {
+		let graph = GKGridGraph(
+			fromGridStartingAt: simd_int2(0, 0),
+			width: 10,
+			height: 10,
+			diagonalsAllowed: false)
+		graph.remove(graph.nodes ?? [])
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
